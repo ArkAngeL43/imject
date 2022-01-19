d87270e5cc0a1e21e9ed454c739a59998b7af9d0
RE43P3R <87203697+ArkAngeL43@users.noreply.github.com>
2022-01-18 22:05:33 -0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,5 @@
+# setup file and mod file
+go get github.com/ArkAngeL43/imject/img
+go get github.com/ArkAngeL43/imject/utility
+go get github.com/ArkAngeL43/imject/modeling
+go get github.com/ArkAngeL43/imject/lib
